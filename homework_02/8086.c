e67e8b8e6c9a9cca6c0675f8dd2efa0edd91d8c6
your name <you@example.com>
2024-04-16 12:08:51 -0700

    Mostly working, still need to finish registers

--- homework_02/8086.c
+++ homework_02/8086.c
@@ -4,10 +4,15 @@
 
 typedef struct {
     uint16_t opcode;
+    uint16_t op_carry;
     uint8_t d_bit : 1;
     uint8_t w_bit: 1;
     uint8_t reg: 3;
     uint8_t rm: 3;
+    uint8_t mod_field: 2;
+    int16_t displacement_16;
+    int8_t displacement_8;
+    int16_t data;
 } opcode_t;
 
 typedef struct {
@@ -20,6 +25,12 @@ void disassemble(chip_t *chip, char *filename) {
 
     char *w_true[] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
     char *w_false[] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
+
+    char *mod_00[] = {"bx + si", "bx + di", "bp + si", "bp + di", "si", "di", "direct", "bx"};
+    char *mod_01[] = {"bx + si ", "bx + di ", "bp + si ", "bp + di", "si", "di", "bp", "bx"};
+    char *mod_10[] = {"bx + si", "bx + di", "bp + si", "bp +_ di", "si", "di", "bp", "bx"};
+    char *mod_11_w[] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};    // w_bit = 1
+    char *mod_11[] = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};      // w_bit = 0;
 
     opcode_t opcode = {0};
 
@@ -46,18 +57,54 @@ void disassemble(chip_t *chip, char *filename) {
         opcode.opcode = (chip->memory[chip->pc] << 8) | chip->memory[chip->pc + 1];
         chip->pc += 2;
 
-        if ((opcode.opcode >> 10) == 0b100010) {
-            opcode.d_bit = (opcode.opcode >> 9) & 0x01;
-            opcode.rm = (opcode.opcode & 0x07);
-            opcode.reg = (opcode.opcode & 0x38) >> 3;
-            opcode.w_bit = (opcode.opcode >> 8) & 0x01;
+        if ((opcode.opcode >> 12) == 0b1011) {
+            opcode.w_bit = (opcode.opcode >> 11) & 0x01;
+            opcode.reg = (opcode.opcode >> 8) & 0x07;
+            int8_t immediate_value = opcode.opcode & 0xFF;
 
-            switch (opcode.w_bit) {
-                case 0: printf("mov %s, %s\n", w_false[opcode.rm], w_false[opcode.reg]); break;
-                case 1: printf("mov %s, %s\n", w_true[opcode.rm], w_true[opcode.reg]); break;
-            }
-        }
+            if (opcode.w_bit == 0) {
+                printf("mov %s, %d\n", w_false[opcode.reg], immediate_value);
+            } else if (opcode.w_bit == 1) {
+                opcode.data = ((chip->memory[chip->pc]) << 8) | (opcode.opcode & 0xFF);
+                printf("mov %s, %d\n", w_true[opcode.reg], opcode.data);
+                chip->pc += 1;
+            } 
+        } else if ((opcode.opcode >> 10) == 0b100010) {
+            opcode.w_bit = (opcode.opcode >> 8) & 0x01;
+            opcode.reg = (opcode.opcode & 0x38) >> 3;
+            opcode.mod_field = (opcode.opcode >> 6) & 0x3;
+            opcode.rm = (opcode.opcode & 0x07);
+            opcode.d_bit = (opcode.opcode >> 9) & 0x01;
+
+            if (opcode.mod_field == 0b00) {
+
+                if (opcode.rm == 0b110) {
+                    opcode.displacement_16 = (chip->memory[chip->pc+1] << 8) | chip->memory[chip->pc];
+                    printf("Unimplemented: %b\n", opcode.displacement_16);
+                    chip->pc += 2;
+
+                } else {
+                    printf("mov [%s]\n", mod_00[opcode.rm]);
+                }
+            } else if (opcode.mod_field == 0b01) {
+                    opcode.displacement_8 = chip->memory[chip->pc];
+                    printf("mov [%s + %d]\n", mod_01[opcode.rm], opcode.displacement_8);
+                    chip->pc += 1;
+            } else if (opcode.mod_field == 0b10) {
+                    opcode.displacement_16 = (chip->memory[chip->pc+1] << 8) | chip->memory[chip->pc];
+                    printf("mov %s, [%s + %d]\n", mod_11[opcode.reg], mod_10[opcode.rm] ,opcode.displacement_16);
+                    chip->pc += 2;
+            } else if (opcode.mod_field == 0b11) {
+                if (opcode.w_bit == 0) {
+                    printf("mov %s, %s\n", mod_11[opcode.rm], mod_11[opcode.reg]);
+                } else {
+                    printf("mov %s, %s\n", mod_11_w[opcode.rm], mod_11_w[opcode.reg]);
+                }
+            } 
+        } 
     }
+    // printf("\n\n");
+
     free(chip->memory);
 }
 
---
+++ homework_02/Makefile
@@ -0,0 +1,2 @@
+8086: 8086.c
+	$(CC) 8086.c -o chip -Wall -Wextra
